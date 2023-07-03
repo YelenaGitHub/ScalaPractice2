710abd10dec9f4759db850990a8e0e7ff285ac23
Elena Druzhilova <edruzhilova@apple.com>
2023-07-03 17:15:50 +0100

    AddTwoLists

--- src/main/scala/collections/lists/linked/AddTwoLists.scala
+++ src/main/scala/collections/lists/linked/AddTwoLists.scala
@@ -1,14 +1,13 @@
 package collections.lists.linked
 
-import java.time.LocalDateTime
 import scala.annotation.tailrec
 
 object AddTwoLists extends App {
 
-  /*
-  You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
-  You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-   */
+/*
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ */
 //
 //  Input: l1 = [2,4,3], l2 = [5,6,4]
 //  Output: [7,0,8]
@@ -47,7 +46,7 @@ def addTwoNumbers(l1: ListNode, l2: ListNode, reminder: Int = 0): ListNode = {
     }
   }
 }
-  
+
 // optimized, uses node1 and node2 for storage
 //  def addTwoNumbers(l1: ListNode, l2: ListNode, reminder: Int = 0): ListNode = {
 //    var i = 0
