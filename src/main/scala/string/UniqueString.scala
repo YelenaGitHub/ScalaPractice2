e44ac05c7e79c01f1e3f915276df03913bc058c1
Elena Druzhilova <edruzhilova@apple.com>
2023-07-09 12:43:01 +0100

    Unique String

--- src/main/scala/string/UniqueString.scala
+++ src/main/scala/string/UniqueString.scala
@@ -2,19 +2,22 @@ package string
 
 object UniqueString extends App {
 
+//  def isUnique(s: String): Boolean = {
+//    s.foldLeft(true, Set[Char]()) {
+//      case ( (_, seen), e) if seen(e) => return false
+//      case ( (_, seen), e) => (true, seen + e)
+//    }._1
+//
+//  }
+
   def isUnique(s: String): Boolean = {
-
-    s.foldLeft(true, Set[Char]()) {
-      case ( (_, seen), e) if seen(e) => return false
-      case ( (_, seen), e) => (true, seen + e)
-    }._1
-
+    val seen = scala.collection.mutable.Set[Char]()
+    s.takeWhile(c => seen.add(c)).length == s.length
   }
 
-
-  val str = "12345" // true
+//  val str = "12345" // true
 //  val str = "123455" // false
-//  val str = "112345" // false
+  val str = "112345" // false
   val res = isUnique(str)
   println(res)
 
